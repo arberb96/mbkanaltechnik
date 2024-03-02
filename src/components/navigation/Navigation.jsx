f3a860bf6e8ed93b3aa8ca37ef1768a7fe78d3e3
arberb96 <biljaliarber@gmail.com>
2024-03-02 17:34:24 +0100

    last changes

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
-import './navigation.css'
+import React, { useState } from 'react';
+import './navigation.css';
 
 function Navigation() {
+  const [menuActive, setMenuActive] = useState(false);
+
   const toggleMenu = () => {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('menu-active');
-};
-  
+    setMenuActive(!menuActive);
+  };
+
+  const handleLinkClick = () => {
+    setMenuActive(false);
+  };
 
   return (
     <div className='header'>
-    <nav className="navigation" >
-        <a href="#hero" className="logo"><span>MB</span>Kanaltechnik</a>
+      <nav className="navigation">
+        <a href="#hero" className="logo" onClick={handleLinkClick}><span>MB</span>Kanaltechnik</a>
 
-        <div className="hamburger-menu" onClick={toggleMenu}>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className={`hamburger-menu ${menuActive ? 'active' : ''}`} onClick={toggleMenu}>
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
         </div>
         
-        <ul className="menu">
-            <li><a href="#our-services">Unsere Dienstleistungen</a></li>
-            <li><a href="#why-choose-us">Warum wir?</a></li>
-            <li><a href="#our-team">Über uns</a></li>
+        <ul className={`menu ${menuActive ? 'menu-active' : ''}`}>
+          <li><a href="#our-services" onClick={handleLinkClick}>Unsere Dienstleistungen</a></li>
+          <li><a href="#why-choose-us" onClick={handleLinkClick}>Warum wir?</a></li>
+          <li><a href="#our-team" onClick={handleLinkClick}>Über uns</a></li>
         </ul>
         
-        <a href="#contact" className="nav-appointment-btn">Kontakt</a>
-    </nav>
+        <a href="#contact" className="nav-appointment-btn" onClick={handleLinkClick}>Kontakt</a>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
